6515623a472650bad9e5157ab059faa7f7666b61
rocka <i@rocka.me>
2017-08-08 00:16:09 +0800

    util: url templates, request method `postKey`
    * url template for getting device page html source
    * post key-value to `link.php`

--- app/src/main/kotlin/me/rocka/mokeedelta/util/Request.kt
+++ app/src/main/kotlin/me/rocka/mokeedelta/util/Request.kt
@@ -3,6 +3,7 @@
 
 package me.rocka.mokeedelta.util
 
+import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
@@ -10,12 +11,36 @@ import java.net.SocketTimeoutException
 
 object Request {
 
+    val baseURL = "https://download.mokeedev.com"
+    val linkPHP = "$baseURL/link.php"
     val client = OkHttpClient()
+
+    fun deviceLink(device: String): String = "$baseURL/?device=$device"
 
     fun get(url: String): String? {
         val req = Request.Builder()
                 .url(url)
                 .get()
+                .build()
+        try {
+            val res = client.newCall(req).execute()
+            return res.body().string()
+        } catch (e: SocketTimeoutException) {
+
+        } catch (e: IOException) {
+
+        }
+        return null
+    }
+
+    fun postKey(key: String): String? {
+        val postBody = MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("key", key)
+                .build()
+        val req = Request.Builder()
+                .url(linkPHP)
+                .post(postBody)
                 .build()
         try {
             val res = client.newCall(req).execute()
