e653e40736765014fbdbf25ee08a3a0a584073d7
rocka <i@rocka.me>
2017-08-09 22:58:21 +0800

    layout & ui: bind data to MainActivity
    MainActivity now shows device name and current system version, also a simple package list

--- app/src/main/kotlin/me/rocka/mokeedelta/activity/MainActivity.kt
+++ app/src/main/kotlin/me/rocka/mokeedelta/activity/MainActivity.kt
@@ -1,23 +1,68 @@
 package me.rocka.mokeedelta.activity
 
+import android.content.Context
+import android.databinding.DataBindingUtil
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import me.rocka.mokeedelta.R
+import me.rocka.mokeedelta.adapter.RomPackageAdapter
+import me.rocka.mokeedelta.databinding.ActivityMainBinding
+import me.rocka.mokeedelta.model.IRomPackage
 import me.rocka.mokeedelta.util.BuildProp
+import me.rocka.mokeedelta.util.Constant
+import me.rocka.mokeedelta.util.Parser
+import me.rocka.mokeedelta.util.Request
 import org.jetbrains.anko.alert
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
+    private val preference by lazy {
+        getSharedPreferences(Constant.MAIN_CONFIG, Context.MODE_PRIVATE)
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var deviceName: String
+    private lateinit var pkgListView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(toolbar)
+        pkgListView = find<RecyclerView>(R.id.main_package_list)
+        binding.currentPkg = Parser.parseCurrentVersion(BuildProp.get("ro.mk.version")!!)
 
-        fab.setOnClickListener { }
+        deviceName = preference.getString(Constant.DEVICE_NAME, Constant.VALUE_NULL)
+        when (deviceName) {
+            Constant.VALUE_NULL -> {
+                preference.edit().apply {
+                    putString(Constant.DEVICE_NAME, Build.DEVICE)
+                    apply()
+                }
+            }
+            is String -> Unit
+        }
+
+        val llm = LinearLayoutManager(this)
+        llm.orientation = LinearLayoutManager.VERTICAL
+        pkgListView.layoutManager = llm
+        pkgListView.adapter = RomPackageAdapter(ArrayList<IRomPackage>())
+
+        fab.setOnClickListener {
+            doAsync {
+                val html = Request.get(Request.deviceLink(deviceName))
+                val pkgList = Parser.parseFullPkg(html!!)
+                uiThread { pkgListView.adapter = RomPackageAdapter(pkgList) }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/kotlin/me/rocka/mokeedelta/util/Constant.kt
@@ -0,0 +1,10 @@
+package me.rocka.mokeedelta.util
+
+// 2017-08-05_MokeeDelta/me.rocka.mokeedelta.util
+// rocka, 17-8-9
+
+object Constant {
+    val MAIN_CONFIG = "MAIN_CONFIG"
+    val DEVICE_NAME = "DEVICE_NAME"
+    val VALUE_NULL = "VALUE_NULL"
+}
