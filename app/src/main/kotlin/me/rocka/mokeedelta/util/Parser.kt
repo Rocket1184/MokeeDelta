6325972fc6a252992fe929b8285855fc1c31385d
rocka <i@rocka.me>
2017-08-09 14:57:46 +0800

    util: [Parser] add `parseCurrentVersion`, fix device parsing

--- app/src/main/kotlin/me/rocka/mokeedelta/util/Parser.kt
+++ app/src/main/kotlin/me/rocka/mokeedelta/util/Parser.kt
@@ -6,14 +6,16 @@ package me.rocka.mokeedelta.util
 import me.rocka.mokeedelta.model.DeltaPackage
 import me.rocka.mokeedelta.model.Device
 import me.rocka.mokeedelta.model.FullPackage
+import me.rocka.mokeedelta.model.ReleaseChannel
 
 object Parser {
 
+    val deltaRoot = "https://download.mokeedev.com/"
     val deviceRe = Regex("""<li id="device_([^"]+)">[^<]+[^>]+><span>([^ ]+)[^<]+</span></a>""")
     val fullPkgRe = Regex("""<td><a href="javascript:downloadPost\('download.php', ?\{key:'([^']+)'[^>]+>([^<]+)</a?><br ?/><small>md5sum: ([^&]+)&nbsp;<a href="([^"]+)">[^<]+</a></small></td>[^<]+<td>([^<]+)</td>""")
     val deltaPkgRe = Regex("""<tr>[^<]+<td><a href="javascript:downloadPost\('download.php', ?\{key:'([^']+)'[^>]+>([^<]+)</a><br/><small>md5sum: ([^<]+)</small></td>[^<]+<td>([^<]+)</td>""")
-    val fullPkgFilenameRe = Regex("""(MK\d+\.\d+)-([^-])+-(\d+)-(\w+)""")
-    val deltaPkgFilenameRe = Regex("""OTA-(MK\d+\.\d+)-([^-])+-(\d+)-(\d+)-(\w+)""")
+    val fullPkgFilenameRe = Regex("""(MK\d+\.\d+)-([^-]+)-(\d+)-(\w+)""")
+    val deltaPkgFilenameRe = Regex("""OTA-(MK\d+\.\d+)-([^-]+)-(\d+)-(\d+)-(\w+)""")
 
     fun parseDevices(input: String): List<Device> {
         val result = deviceRe.findAll(input)
@@ -24,21 +26,38 @@ object Parser {
         return output
     }
 
+    fun parseCurrentVersion(input: String): FullPackage {
+        val result = fullPkgFilenameRe.find(input)
+        return FullPackage(
+                device = result?.groupValues?.get(2) ?: "",
+                model = "",
+                fileName = "$input.zip",
+                md5sum = "",
+                deltaUrl = "",
+                key = "",
+                version = result?.groupValues?.get(3) ?: "",
+                size = "",
+                url = "",
+                channel = ReleaseChannel.valueOf(result?.groupValues?.get(4) ?: ReleaseChannel.UNKNOWN.toString())
+        )
+    }
+
     fun parseFullPkg(input: String): List<FullPackage> {
         val result = fullPkgRe.findAll(input)
         val output = ArrayList<FullPackage>()
         result.forEach {
             val versionResult = fullPkgFilenameRe.find(it.groupValues[2])
             output.add(FullPackage(
-                    device = "",
+                    device = versionResult!!.groupValues[2],
                     model = "",
                     fileName = it.groupValues[2],
                     md5sum = it.groupValues[3],
-                    deltaUrl = "https://download.mokeedev.com/${it.groupValues[4]}",
+                    deltaUrl = "$deltaRoot${it.groupValues[4]}",
                     key = it.groupValues[1],
-                    version = versionResult?.groupValues!![3],
+                    version = versionResult.groupValues[3],
                     size = it.groupValues[5],
-                    url = ""
+                    url = "",
+                    channel = ReleaseChannel.valueOf(versionResult.groupValues[4])
             ))
         }
         return output
@@ -50,14 +69,14 @@ object Parser {
         result.forEach {
             val versionResult = deltaPkgFilenameRe.find(it.groupValues[2])
             output.add(DeltaPackage(
-                    device = "",
+                    device = versionResult!!.groupValues[2],
                     model = "",
                     fileName = it.groupValues[2],
                     md5sum = it.groupValues[3],
                     key = it.groupValues[1],
                     size = it.groupValues[4],
                     url = "",
-                    base = versionResult?.groupValues!![3],
+                    base = versionResult.groupValues[3],
                     target = versionResult.groupValues[4]
             ))
         }
