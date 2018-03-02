ea25f1dab4d2ed826ef6d01f102675286232686a
Rocka <i@rocka.me>
2018-03-02 21:04:08 +0800

    model: add PostFilePayload

---
+++ app/src/main/kotlin/me/rocka/mokeedelta/model/PostFilePayload.kt
@@ -0,0 +1,11 @@
+// MokeeDelta/me.rocka.mokeedelta.util
+// rocka, 18-3-2
+
+package me.rocka.mokeedelta.model
+
+data class PostFilePayload(
+        val key: String,
+        val device: String,
+        val type: String,
+        val owner: String
+)
